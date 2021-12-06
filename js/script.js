d6be46e38b1bd8d673be724612e27cae530e9035
marikhdkv <mari_skl1212@mail.ru>
2021-12-07 00:07:25 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function slowScroll(id) {
+    $('html, body').animate({
+        scrollTop: $(id).offset().top
+    }, 500);
+}
+
+$(document).on("scroll", function () {
+    if ($(window).scrollTop() === 0)
+        $("header").removeClass("fixed");
+    else
+        $("header").attr("class", "fixed");
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var splide = new Splide('.splide');
+    splide.mount();
+});
